9973e9f623372eed43af0b042a178b5c390e7a84
Andre Susantin <andre.susantin@google.com>
2021-08-29 17:20:22 -0700

    aug21

--- series/series.rb
+++ series/series.rb
@@ -4,22 +4,8 @@ class Series
         @serial = serial.chars
     end
     def slices(n)
-        if serial.length < n 
-            raise ArgumentError
-        end 
-        # (0..serial.length-n).map { |i| serial[i..i+n-1].join }
-        # (0..serial.length-n).map do |i| 
-        #     serial[i..i+n-1].join 
-        # end
-
-        # (serial).map.with_index do |e,i| 
-        #     next if i + n > serial.length
-        #         # serial[i..i+n-1].join
-                
-        #     serial.slice(i...).take(n).join           
-        # end
+        raise ArgumentError if serial.length < n 
         serial.each_cons(n).map(&:join)
-
     end
 end
 
